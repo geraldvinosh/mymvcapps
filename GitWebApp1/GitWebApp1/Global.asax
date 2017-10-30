ec2eee828623fd3b2d685f75adf32b4cf97362c9
Gerald <geraldvinosh@live.com>
2017-10-30 12:17:46 +0530

    First Commit

---
+++ GitWebApp1/GitWebApp1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GitWebApp1.MvcApplication" Language="C#" %>
---
+++ GitWebApp1/GitWebApp1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitWebApp1.Startup))]
+namespace GitWebApp1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
